b5b19ba62d86e50138ef48eae9f63dc0602af611
Tobias Koppers <tobias.koppers@googlemail.com>
2012-04-06 12:30:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# json loader for webpack
+
+## Usage
+
+``` javascript
+var json = require("json!./file.json");
+// => returns file.json content as json parsed object
+```
+
+Don't forget to polyfill `require` if you want to use it in node.
+See `webpack` documentation.
+
+## License
+
+MIT (http://www.opensource.org/licenses/mit-license.php)
---
+++ index.js
@@ -0,0 +1,17 @@
+/*
+	MIT License http://www.opensource.org/licenses/mit-license.php
+	Author Tobias Koppers @sokra
+*/
+module.exports = function() {
+	var values = this.values = [];
+	var options = this;
+	var args = Array.prototype.slice.call(arguments);
+	args.forEach(function(str, idx) {
+		values[idx] = JSON.parse(str);
+	});
+	var results = [null];
+	values.forEach(function(jsobj, idx) {
+		results[idx+1] = "module.exports = " + JSON.stringify(jsobj, undefined, "\t");
+	});
+	this.callback.apply(null, results);
+}
