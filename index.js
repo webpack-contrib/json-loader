994c46c9c264c0320f6f5ad831dad0171f26dd91
Tobias Koppers <tobias.koppers@googlemail.com>
2012-08-03 12:01:45 +0200

    fix to standard interface

--- index.js
+++ index.js
@@ -4,15 +4,7 @@
 */
 module.exports = function() {
 	this.cacheable && this.cacheable();
-	var values = this.values = [];
-	var options = this;
-	var args = Array.prototype.slice.call(arguments);
-	args.forEach(function(str, idx) {
-		values[idx] = JSON.parse(str);
-	});
-	var results = [null];
-	values.forEach(function(jsobj, idx) {
-		results[idx+1] = "module.exports = " + JSON.stringify(jsobj, undefined, "\t");
-	});
-	this.callback.apply(null, results);
+	var value = JSON.parse(source);
+	this.values = [value];
+	return "module.exports = " + JSON.stringify(value, undefined, "\t");
 }
