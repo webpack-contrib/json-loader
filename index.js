dcd541d19f3acb3921849ffb7d6388991d051e45
Dan Forster <dan@simpleseed.com.au>
2015-05-27 01:16:27 +0200

    added semicolon to end of the result assignment statement

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ module.exports = function(source) {
 	this.cacheable && this.cacheable();
 	var value = JSON.parse(source);
 	this.value = [value];
-	return "module.exports = " + JSON.stringify(value, undefined, "\t");
+	return "module.exports = " + JSON.stringify(value, undefined, "\t") + ";";
 }
