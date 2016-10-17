ee75f99197114cc0f6ba0cc56c2c85173176c6ec
Kees Kluskens <kees@webduck.nl>
2016-10-17 16:57:17 +0200

    fix: Don't stringify with tabs to improve perf
    Fixes #31

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ module.exports = function(source) {
 	this.cacheable && this.cacheable();
 	var value = typeof source === "string" ? JSON.parse(source) : source;
 	this.value = [value];
-	return "module.exports = " + JSON.stringify(value, undefined, "\t") + ";";
+	return "module.exports = " + JSON.stringify(value) + ";";
 }
