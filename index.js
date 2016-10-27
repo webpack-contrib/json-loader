67a80e1f6c21a73981e24ede0e265a6714fd5c3c
Kees Kluskens <kees@webduck.nl>
2016-10-27 11:36:25 +0200

    Export an ES Module instead of CommonJS module
    Since webpack v2, it's better to use ES modules. Note that this is incompatible with webpack v1, so it should be released as a major version.

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ module.exports = function(source) {
 	this.cacheable && this.cacheable();
 	var value = typeof source === "string" ? JSON.parse(source) : source;
 	this.value = [value];
-	return "module.exports = " + JSON.stringify(value) + ";";
+	return "export default " + JSON.stringify(value) + ";";
 }
