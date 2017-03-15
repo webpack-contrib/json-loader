dbf1fa5a8ecdb08089a1005f175075414e70f187
Rabbit <yfhj1990@hotmail.com>
2017-03-15 13:36:55 +0800

    fix: add `stringify` option to output JSON object as string (#45)

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 /*
-	MIT License http://www.opensource.org/licenses/mit-license.php
-	Author Tobias Koppers @sokra
+  MIT License http://www.opensource.org/licenses/mit-license.php
+  Author Tobias Koppers @sokra
 */
+var loaderUtils = require('loader-utils');
+
 module.exports = function(source) {
-	this.cacheable && this.cacheable();
-	var value = typeof source === "string" ? JSON.parse(source) : source;
-	this.value = [value];
-	return "module.exports = " + JSON.stringify(value) + ";";
+    var value = typeof source === "string" ? JSON.parse(source) : source;
+    var options = loaderUtils.getOptions(this) || {};
+    value = JSON.stringify(value)
+    value = options.stringify ? `'${value}'` : value
+    var module = this.version && this.version >= 2 ? `export default ${value};` : `module.exports = ${value};`;
+    return module
 }
