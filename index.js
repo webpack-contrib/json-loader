3b3069fa3f9deb35cc625c6c388e234a240ee312
Andrew Hyndman <ajhyndman@hotmail.com>
2017-03-18 09:26:30 +1100

    fix: escape newline/paragraph separators (#18)

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ module.exports = function(source) {
     var value = typeof source === "string" ? JSON.parse(source) : source;
     var options = loaderUtils.getOptions(this) || {};
     value = JSON.stringify(value)
+        .replace(/\u2028/g, '\\u2028')
+        .replace(/\u2029/g, '\\u2029');
     value = options.stringify ? `'${value}'` : value
     var module = this.version && this.version >= 2 ? `export default ${value};` : `module.exports = ${value};`;
     return module
